7d085f49ef9bcbd5c1dd714b05e70e5fc7818659
qcorreia <quentin@cirqll.nl>
2024-12-07 17:20:22 +0100

    refactor: remove commented out code

--- test/serialize.test.ts
+++ test/serialize.test.ts
@@ -36,37 +36,4 @@ describe('serialize', () => {
 
         expect(JSON.stringify(parsed)).toEqual(json);
     });
-
-    // test('stringify', () => {
-    //     const result = new Chain()
-    //         .add('select', 'h1')
-    //         .or(chain => chain.add('select', 'h2'))
-    //         .or(chain => chain.add('select', 'h3'))
-    //         .or(chain => chain.add('select', 'h3').add('first'))
-    //         .or(chain => chain
-    //             .add('select', 'h3')
-    //             .or(chain => chain.add('select', 'h4')))
-    //         .add('first')
-    //         .add('text');
-
-    //     expect(JSON.parse(JSON.stringify(result))).toEqual({
-    //         chain: [{
-    //             or: [{
-    //                 action: 'select',
-    //                 options: 'h1',
-    //             },
-    //             {action: 'select', options: 'h2'},
-    //             {action: 'select', options: 'h3'},
-    //             {
-    //                 chain: [
-    //                     {action: 'select', options: 'h3'},
-    //                     {action: 'first'},
-    //                 ],
-    //             },
-    //             {or: [{action: 'select', options: 'h3'}, {action: 'select', options: 'h4'}]}],
-    //         },
-    //         {action: 'first'},
-    //         {action: 'text'}],
-    //     });
-    // });
 });
