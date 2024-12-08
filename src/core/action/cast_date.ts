c87d47c715bc23cc4ab548461d194a59e805e95c
Quentin Correia <qcorreia@mostwanted.io>
2024-12-08 17:36:47 +0100

    feat: add cast_date action

---
+++ src/core/action/cast_date.ts
@@ -0,0 +1,9 @@
+import {parse} from 'date-fns';
+import type {Locale} from 'date-fns/locale';
+
+/**
+ * https://date-fns.org/v4.1.0/docs/parse
+ */
+export function cast_date(_context: any, value: string, format = 'MM/dd/yyyy', reference_date: Date = new Date(), locale?: Locale): Date {
+    return parse(value, format, reference_date, {locale});
+}
